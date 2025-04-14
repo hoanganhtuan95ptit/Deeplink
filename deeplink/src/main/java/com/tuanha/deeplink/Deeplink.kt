7da1a59d0cc0a55b21ce2c178f39ac3439c78671
Hoàng Anh Tuấn <hoanganhtuan95ptit@gmail.com>
2025-04-13 20:52:58 -0700

    Create Deeplink.kt

---
+++ deeplink/src/main/java/com/tuanha/deeplink/Deeplink.kt
@@ -0,0 +1,17 @@
+@file:Suppress("FoldInitializerAndIfToElvis")
+
+package com.tuanha.deeplink
+
+import android.os.Bundle
+import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+fun sendDeeplink(deepLink: String, extras: Bundle? = null, sharedElement: Map<String, View>? = null) = CoroutineScope(Dispatchers.Main.immediate).launch {
+
+    if (!flow.replayCache.toMap().containsKey(deepLink)) {
+
+        flow.emit(deepLink to (extras to sharedElement))
+    }
+}
