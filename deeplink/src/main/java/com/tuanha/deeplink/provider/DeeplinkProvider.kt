4954f0c4a53d57ba5b3165bb5a418ab3d69cee97
Hoàng Anh Tuấn <hoanganhtuan95ptit@gmail.com>
2025-04-13 20:50:59 -0700

    Update DeeplinkProvider.kt

--- deeplink/src/main/java/com/tuanha/deeplink/provider/DeeplinkProvider.kt
+++ deeplink/src/main/java/com/tuanha/deeplink/provider/DeeplinkProvider.kt
@@ -2,6 +2,6 @@ package com.tuanha.deeplink.provider
 
 import com.tuanha.deeplink.DeeplinkHandler
 
-interface DeeplinkProvider {
-    fun provider(): List<DeeplinkHandler>
+abstract class DeeplinkProvider {
+   abstract fun provider(): List<Pair<String, DeeplinkHandler>>
 }
